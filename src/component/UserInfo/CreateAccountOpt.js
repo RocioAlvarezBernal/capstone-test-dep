c51552d7d1b30d04acd48298b9298626ee3f40a2
Rocio Alvarez Bernal <dev.alvarezbernal@gmail.com>
2021-02-04 13:21:16 -0600

    toggle onclick user account management

--- src/component/UserInfo/CreateAccountOpt.js
+++ src/component/UserInfo/CreateAccountOpt.js
@@ -1,9 +1,37 @@
 import React from 'react'
 
 class CreateAccountOpt extends React.Component {
-    render(){
-        return(
+
+    constructor(){
+        super()
+        this.state={
+            content:null,
+            inDisplayMode: false
+        }
+    }
+
+    displayMode=()=>{
+        this.setState(
+            {
+                inDisplayMode: !this.state.inDisplayMode
+            }
+        )
+    }
+
+    updateValue=()=>{
+        this.setState(
+            {
+                inDisplayMode: false,
+                content: this.refs.textInput.value
+            }
+        )
+    }
+
+    renderDisplayMode = () => {
+        return (
             <div>
+                <h4>Select the type of account:</h4>
+                <div>
                 <label for="Account">Select the type of account</label>
                 <select id="acct" name="acct">
                 <option value="Savings">Savings</option>
@@ -11,14 +39,33 @@ class CreateAccountOpt extends React.Component {
                 <option value="CD">CD</option>
                 <option value="IRA">IRA</option>
                 </select>
-            <form>
-                <input type='text'></input>
-                <button>Submit</button>
-            </form>
+            </div>
 
+            <LISbutton 
+                text= 'Submit'
+                disabled={this.props.disabled}
+                onClick={this.updateValue}
+                className='lisb'
+            />
             </div>
         )
     }
+
+    renderDefaultView=()=>{
+        return(
+            <div>
+  
+            </div>
+        )
+    }
+
+
+
+    render(){
+
+        return this.state.inDisplayMode ? 
+            this.renderDisplayMode():this.renderDefaultView()
+        }
 }
 
 export default CreateAccountOpt 
--- src/component/pages/SettingsPage.js
+++ src/component/pages/SettingsPage.js
@@ -8,15 +8,41 @@ class SettingsPage extends React.Component {
         return(
             <div className = 'container'>
             <main id='SettingsPageMain' >
-
-
                 <UserInformation />
-
                 <AccountManagement />
             </main>
             </div>
         )
     }
 }
+
+
+// render(){
+//     return(
+//         <div className='signInForm'>
+//             Log In
+//             <InputField
+//              type="text" 
+//              placeholder="Username"
+//              value = { this.state.username ? this.state.username: ''}
+//              onChange = { (val) => this.setInputValue('username' , val)}
+//              />   
+
+//             <InputField
+//              type="password" 
+//              placeholder="Password"
+//              value = { this.state.password ? this.state.password : ''}
+//              onChange = { (val) => this.setInputValue('password' , val)}
+//              />   
+
+//             <LISbutton 
+//                 text= 'Sign in'
+//                 disabled={this.state.buttonDisables}
+//                 onClick ={ () => this.doLogin()}
+//                 className='lisb'
+//             />
+//         </div>
+//     )
+// }
 
 export default SettingsPage  
